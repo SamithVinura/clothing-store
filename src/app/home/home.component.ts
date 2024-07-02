9b36ebfc4e563c0a51c62d4329a24aaf2f96b88c
Samith Vinura <104480599+SamithVinura@users.noreply.github.com>
2024-07-02 12:01:16 +0530

    product showing ui and functions implemented, edit add delete api and fucntions implemented

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,82 @@
+import { Component } from '@angular/core';
+import { ProductsService } from '../services/products.service';
+import { Product, Products } from '../../types';
+import { ProductComponent } from '../components/product/product.component';
+import { CommonModule } from '@angular/common';
+import { PaginatorModule } from 'primeng/paginator';
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [ProductComponent, CommonModule, PaginatorModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+})
+export class HomeComponent {
+  constructor(private productService: ProductsService) {}
+  products: Product[] = [];
+  totalRecords: number = 0;
+  rows: number = 5;
+  onProductOutput(product: Product) {
+    console.log(product);
+  }
+
+  onPageChange(event: any) {
+    this.fetchProducts(event.page, event.rows);
+  }
+
+  fetchProducts(page: number, perPage: number) {
+    this.productService
+      .getProducts('http://localhost:3000/clothes', { page, perPage })
+      .subscribe({
+        next: (products: Products) => {
+          this.products = products.items;
+          this.totalRecords = products.total;
+        },
+        error: (error) => {
+          console.log(error);
+        },
+      });
+  }
+
+  addProduct(product: Product) {
+    this.productService
+      .addProduct(`http://localhost:3000/clothes`, product)
+      .subscribe({
+        next: (data) => {
+          console.log(data);
+           this.fetchProducts(0, this.rows);
+        },
+        error: (error) => {
+          console.log(error);
+        },
+      });
+  }
+  editProduct(product: Product,id:number) {
+    this.productService
+      .editProduct(`http://localhost:3000/clothes/${id}`, product)
+      .subscribe({
+        next: (data) => {
+          console.log(data);
+           this.fetchProducts(0, this.rows);
+        },
+        error: (error) => {
+          console.log(error);
+        },
+      });
+  }
+  deleteProduct(product: Product,id:number) {this.productService
+      .deleteProduct(`http://localhost:3000/clothes/${id}`)
+      .subscribe({
+        next: (data) => {
+          console.log(data);
+           this.fetchProducts(0, this.rows);
+        },
+        error: (error) => {
+          console.log(error);
+        },
+      });}
+
+  ngOnInit() {
+    this.fetchProducts(0, this.rows);
+  }
+}
---
+++ src/app/services/products.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { ApiService } from './api.service';
+import { Observable } from 'rxjs';
+import { PaginationParams } from '../../types';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+
+  constructor(private apiService:ApiService) { }
+
+  getProducts = (url:string,params:PaginationParams):Observable<any>=>{
+    return this.apiService.get(url,{
+      params,
+      responseType:'json'
+    })
+  }
+
+  // Adding a product via the API
+  addProduct = (url: string, body: any): Observable<any> => {
+    return this.apiService.post(url, body, {});
+  };
+
+  // Editing a product via the API
+  editProduct = (url: string, body: any): Observable<any> => {
+    return this.apiService.put(url, body, {});
+  };
+
+  // Deleting a product via the API
+  deleteProduct = (url: string): Observable<any> => {
+    return this.apiService.delete(url, {});
+  };
+}
